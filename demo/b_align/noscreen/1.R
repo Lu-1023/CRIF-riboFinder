25d0e5eee242e8c6c3559d50a12169acac962722
Lu-1023 <118334513+Lu-1023@users.noreply.github.com>
2022-12-31 16:25:28 +0800

    Add files via upload

---
+++ demo/b_align/noscreen/1.R
@@ -0,0 +1,87 @@
+library(qvalue)
+library(readr)
+library(ggrepel)
+setwd("/media/hp/disk1/lu/demo/b_align/noscreen/FS")
+samples<-system("ls /media/hp/disk1/lu/demo/b_align/noscreen/coverage_stat/*_RPF_count.txt",intern=T)
+#RPF aim to add the RPKM and shift_ratio
+RPF <- function(sam,f)  {
+  #sam:the path of the RPF_count.txt, f:frame shifting sorts<1 or 2>
+  x<-gsub(".*/(.*)_RPF_count.txt","\\1",sam)
+  d <- read.table(sam, sep="\t", header = T)
+  d$ORF_length<-d$ORF_length-200
+  # calculate the number codon that are covered by at least 1 read
+  d$inframe_codon_cover_number = ceiling(d$inframe_cov/100 * (d$ORF_length - 200)/3 )
+  # note  hist(d$inframe_codon_cover_number)
+  d$RPF_total = apply(d[,c("inframe_count", "shift_plus_count", "shift_minus_count")], 1, sum)
+  d$RPF_RPKM = d$RPF_total*1000000*1000/sum(d$RPF_total)/(d$ORF_length-200)
+  dfe<-d
+  rm(d)
+  dfe$shift_plus_ratio = dfe$shift_plus_count/dfe$RPF_total
+  dfe$shift_minus_ratio = dfe$shift_minus_count/dfe$RPF_total
+  dfe$shift_ratio = (dfe$shift_minus_count + dfe$shift_plus_count)/dfe$RPF_total
+  df1 <- dfe[order(dfe$shift_ratio),]
+  df1<-transform(df1,num_codon=(ORF_length-200)/3)
+  df1<-transform(df1,cov=inframe_cov+shift_plus_cov+shift_minus_cov)
+  #df1<-df1[df1$inframe_cov>5 & df1$RPF_RPKM>30 & df1$cov*df1$num_codon*0.01 > 20,]
+  #df1<-df1[,c("Gene","RPF_RPKM","shift_ratio")]
+  #or
+  FF<-read.table(paste0(x,"/",x,"frame",f,"_information.txt"),header = T,sep="\t")
+  #A<-read.table(paste0(x,"/",x,"frame",f,"_array.txt"),header = T,sep="\t")
+  #FF<-read.table(paste0(x,"/",x,"frame",f,"_hsc_information.txt"),header = T,sep="\t")
+  total<-merge(FF,df1[,c(1,2,13)],by.x = "id",by.y="Gene",all.x = T)
+  total$padjust<-p.adjust(total$p.value,method = "fdr",n=nrow(total))
+  total_n<-total
+  total<-na.omit(total)
+  total$padjust<-p.adjust(total$p.value,method = "fdr",n=nrow(total))
+  write.csv(total,paste0(x,"/",x,"frame",f,"_information_exp.csv"),row.names=FALSE)
+  
+  #A$id<-paste(A$id,A$start,sep="_")
+  #A<-merge(total,A[,c(1,6,7)],by.x="id",by.y="id")
+  #write.csv(A,paste0(x,"/",x,"frame",f,"_array.csv"),row.names=FALSE)
+  if (f==1){
+    total$color<-ifelse(total$padjust<0.05 & total$foldchange >1.5,"salmon","grey")
+    color<-c("grey","salmon")
+  }else{
+    total$color<-ifelse(total$padjust<0.05 & total$foldchange >1.5,"steelblue3","grey")
+    color<-c("grey","steelblue3")
+  }
+  #total<-total[total$foldchange>1.5,]
+  exp_gene<-"" #c("EFNA3_NM_004952_529","KMT2D_NM_003482_11368","CPSF6_NM_001300947_1030")
+  #total$id<-paste(total$id,total$start,sep="_")
+  
+  #png(filename = paste0(x,"/",x,"_fsgenes",f,"_volcano.png"),width = 1000,height =1000,bg="white")
+  g<-ggplot(data=total,aes(x=log2(foldchange),y=-log10(padjust),color=color))+
+    geom_point(size=3,shape=16)+
+    labs(title = paste("algorism in ",f," frame"),y="-log10(p.adjust)",x= "log2(foldchange)")+
+    scale_color_manual(values =color)+
+    theme(plot.title = element_text(face = "bold",hjust=0.5,size = 40),
+          axis.text.x = element_text(face="italic", color="grey20", size=30),
+          axis.text.y = element_text( color="grey20", size=30),panel.background = element_rect(fill = "transparent",colour = "black",size=2.5),
+          axis.title = element_text(size = 35,family = "Arial",color="grey20"),legend.text=element_text(size=35),
+          legend.title=element_blank())+
+    guides(fill=guide_legend(title=NULL))+
+    scale_y_continuous(breaks = seq(0,25,3),limit=c(0,25))+
+    scale_x_continuous(breaks = seq(-(round(max(log2(total$foldchange))+1)),round(max(log2(total$foldchange)))+1,2),
+                       limit=c(-round(max(log2(total$foldchange))),round(max(log2(total$foldchange)))))+
+    geom_hline(yintercept=-log10(0.05),lty=4,col="black",lwd=0.6)+
+    geom_vline(xintercept=log2(1.5),lty=4,col="black",lwd=0.6)+
+    geom_text_repel(data = subset(total,total$id %in% exp_gene),aes(label=id),size=10,box.padding = unit(10, "lines"),segment.color = "black")
+  ggsave(filename = paste0(x,"/",x,"_fsgenes",f,"_volcano.eps"),plot=g,width = 1000,height =1000,bg="white",units = "px",dpi = 72)                  
+  #print(g)
+  #dev.off()
+  #screen genes based on p-adjust value and foldchange
+  total$color<-NULL
+  total_fs<-total[total$padjust<0.05 &total$foldchange>1.5,]
+  write.csv(total_fs,paste0(x,"/",x,"frame",f,"fs_fc1.csv"),row.names=FALSE)
+  write.table(total_fs,paste0(x,"/",x,"frame",f,"fs_fc1.txt"),quote=FALSE,sep="\t",row.names=FALSE)
+  total_fs$fs_sort<-rep(f,nrow(total_fs))
+  write_csv(total_fs,paste0(x,"/",x,"fs_fc1.csv"),append = T,col_names = T)
+  write.table(total[total$foldchange<1,],paste0(x,"/",x,"frame",f,"nc1.txt"),quote=FALSE,sep="\t",row.names=FALSE)
+  total$fs_sort<-rep(f,nrow(total))
+  write_csv(total,paste0(x,"/",x,"fs_expression.csv"),append = T,col_names = T)
+}
+
+for (i in samples) {
+  RPF(i,1)
+  RPF(i,2)
+}
---
+++ demo/b_align/noscreen/frame_shift_statistic_site.py
@@ -0,0 +1,258 @@
+"""
+This script is used to define the frame-shift sites in repeat motif.I use the independent t.text
+to test whether the baseline(-1 or +1 frame) is different from the repeat box (from the motif_stop
+site to the hidden stop codon). And then compared with the average of the baseline and repeat box ,
+if baseline > repeat box , we see the motif site as the frame-shifting site.
+intensity = (box_avg/all0_avg)-(allfs_avg/all0_avg)
+"""
+from scipy import stats
+import re
+import getopt
+import sys
+import re
+import os
+import numpy
+import argparse
+
+numpy.seterr(divide='ignore',invalid='ignore')
+# get the python script input method1
+# def usage():
+#     print("python frame_shift_statistic_site.py --sp <species> --doc <PATH> -f <frame>\n")
+#     print("<species> : the sample from which species\n<doc> : /media/hp/disk4/shuang/Ribo_seq/XXX\n<frame> : 1 or 2 represents +1 or -1 frame")
+# if len(sys.argv[1:])<1 :
+#     usage()
+#     sys.exit("WRONG,you must type the options firstly!")
+# try:
+#     opt,arg=getopt.getopt(sys.argv[1:],"hs:d:f:",["help","sp=","doc=","frame="])
+# except:
+#     usage()
+#     sys.exit("WRONG,non-existent options you type!")
+# for name,value in opt:
+#     if name in ("-h","--help"):
+#         usage()
+#     if name in ("-s","--sp"):
+#         organism=value
+#     if name in ("-d","--doc"):
+#         sample=value
+#     if name in ("-f","--frame"):
+#         frame=int(value)
+
+# get the python script input method2
+parser=argparse.ArgumentParser(description="the prediction method for CRIF.")
+parser.add_argument("-f","--frame",type=int,metavar="frame",required=True,help="please input 1 or 2 means +1 or -1 frame")
+parser.add_argument("-s","--sp",type=str,metavar="organism",required=True,help="the sample is from which species.")
+parser.add_argument("-d","--doc",type=str,metavar="sample",required=True,help="the location where your files are.pattern like this :/media/hp/disk4/shuang/Ribo_seq/XXX")
+args=parser.parse_args()
+frame=args.frame
+sample=args.doc
+organism=args.sp
+
+#get the CDS_DNA ,and input it to a directory
+gene={}
+cds=open("/media/hp/disk1/lu/demo/CDS_DNA.fa","r")
+for line in cds:
+    line=re.sub(r'\s+$','',line)
+    if (re.match(r'^>',line)):
+        gene_id=re.sub(r'^>','',line)
+        continue
+    gene.update({gene_id:line})
+cds.close()
+
+#get the size of the cds_gene
+global size
+size={}
+for key,value in gene.items() :
+    size.update({key:len(value)})
+
+#function:get the repeat box
+def R_box(x,y,z): #x : motif stop site .y: +1 or -1 hiden stop codon site .z: frame.
+    r_box=[]
+    global num
+    num=0
+    for j in range(x+z,y,3): #?
+        if (id+"_"+str(j) in rpf.keys()):
+           r_box.append(round(rpf[id+"_"+str(j)],1))
+           num+=1
+        else:
+            r_box.append(0)
+    r_box=numpy.array(r_box)
+    #set the dot : numpy.set_printoptions(precision=4) or line 74
+    global r_Len
+    r_Len=(y-x-z)/3 # codon length
+    #take the codon counts in 0 frame at repeat box, and the the medium to normalize repeat box
+    inframe_box=[]
+    global rbox_in_ecodon
+    rbox_in_ecodon=0
+    for j in range(x,y,3):
+        if (id+"_"+str(j) in rpf.keys()):
+           inframe_box.append(round(rpf[id+"_"+str(j)],1))
+           rbox_in_ecodon+=1
+        else:
+            inframe_box.append(0)
+    #we want to get the framshiting expression
+    ratio=numpy.sum(r_box)/r_Len
+    inframe_box_m=numpy.mean(inframe_box)
+    r_box=(r_box*num/r_Len)/inframe_box_m
+    r_box=numpy.round(r_box,6)
+    # remove 0 in r_box to aviod affecting the p-value
+    list0=[]
+    for i in range(0,len(r_box)):
+        if (r_box[i]==0):
+            list0.append(i)
+    r_box=numpy.delete(r_box,list0)
+    rinframe=numpy.sum(inframe_box)/r_Len
+    return r_box,ratio,rinframe
+
+try:
+    os.mkdir(sample+"/b_align/noscreen/FS")
+except:
+    print("file exists!")
+
+#function : get the fs sites using the same hiden stop codon
+def find_seq(x):
+    hsc=open(organism +"_ALL_hsc_position_frame"+str(x)+".txt","r")
+    h={}
+    for l in hsc:
+        if (re.match(r'^id',l)):
+            continue
+        ID,motif,start,stop,same=l.strip("\n").split("\t")
+        if (ID+"_"+same in h.keys()):
+                h[ID + "_" + same].extend([start, stop])
+        else:
+            h.update({ID + "_" + same: [start, stop]})
+    hsc.close()
+    return h
+
+#function : get the down/up reads count as the 2th compare
+def down_up(s,e,f):
+    up = []
+    down = []
+    #jump the whole repeat
+    for a in range(s - 33+f, s, 3):
+        if (id + "_" + str(a) in rpf.keys()):
+            up.append(round(rpf[id + "_" + str(a)], 1))
+        else:
+            up.append(0)
+    # jump the whole repeat
+    for a in range(e+f, e + 30+f, 3):
+        if (id + "_" + str(a) in rpf.keys()):
+            down.append(round(rpf[id + "_" + str(a)], 1))
+        else:
+            down.append(0)
+    return up,down
+
+
+#function : to get the baseline and repeat box
+def pro_bg (x,y):
+    print("===================now processing "+x+" !===================")
+    #get the _A.bg file and make it to a diretoryrpf={}
+    global rpf
+    rpf={}
+    gene_exist=set() #storage the gene id in the sample
+    bg_file=open(y+"/b_align/noscreen/"+x+"_A.bg",'r')
+    for L in bg_file:
+        a=L.strip('\n').split('\t')
+        for i in range(int(a[1]),int(a[2])):
+           if int(a[3])>0 :
+            rpf.update({a[0]+"_"+str(i):float(a[3])})
+            gene_exist.add(a[0])
+    bg_file.close()
+    H=find_seq(frame)
+    fs=open(y+"/b_align/noscreen/FS/"+x+"/"+x+"frame"+str(frame)+".txt",'w')
+    fs.write("id"+"\t"+"motif"+"\t"+"start"+"\t"+"stop"+"\t"+"p-value"+"\t"+"foldchange"+"\n")
+    box=open(y+"/b_align/noscreen/FS/"+x+"/"+x+"frame"+str(frame)+"_repeatbox.txt",'w')
+    inf=open(y+"/b_align/noscreen/FS/"+x+"/"+x+"frame"+str(frame)+"_information.txt",'w')
+    inf.write("id"+"\t"+"motif"+"\t"+"start"+"\t"+"stop"+"\t"+"hsc"+"\t"+"e_codon_inframe"+"\t"+"e_codon_baseline"+"\t"+"e_codon_rbox"+"\t"+"e_codon_rbox_inframe"+"\t"+"rbox_length(codon)"+"\t"+"cds_length(codon)"+"\t"+"rbox_inframe_avg"+"\t"+"p-value"+"\t"+"foldchange"+"\t"+"intensity(%)"+"\n")
+    second=open(y+"/b_align/noscreen/FS/"+x+"/"+x+"frame"+str(frame)+"_compare.txt",'w')
+    second.write("id"+"\t"+"motif"+"\t"+"start"+"\t"+"stop"+"\t"+"hsc"+"\t"+"p-value"+"\t"+"foldchange"+"\t"+"down/up"+"\n")
+    # get the information for the hiden stop codon
+    hsc = open(organism + "_ALL_hsc_position_frame"+str(frame)+".txt", 'r')
+    for l in hsc:
+        l = l.strip('\n')
+        if (re.match(r'^id', l)):
+            continue
+        global id
+        id, motif, start, stop, same= l.split('\t')
+        if (id in gene_exist):
+            baseline=[]
+            global e_count
+            e_count=0
+            for i in range(100+30+frame,size[id]-100-30,3):
+                if (id+"_"+str(i) in rpf.keys()) :
+                    baseline.append(round(rpf[id+"_"+str(i)],1))
+                    e_count+=1
+                else:
+                    baseline.append(0)
+            #value avg(all -1)
+            length = (size[id] - 200-60) / 3  # codon length
+            baseline = numpy.array(baseline)
+            value_allfs=numpy.sum(baseline)/length
+
+            # take the codon counts in 0 frame , and the the medium to normalize baseline
+            inframe=[]
+            in_codon = 0
+            for j in range(100+30,size[id]-100-30,3):
+                if (id+"_"+str(j) in rpf.keys()) :
+                    inframe.append(round(rpf[id+"_"+str(j)],1))
+                    in_codon+=1
+                else:
+                    inframe.append(0)
+            inframe_m=numpy.mean(inframe)
+            # value avg(all0)
+            value_all0 = numpy.sum(inframe) / length
+
+            #array baseline
+            baseline=(baseline*e_count/length)/inframe_m
+            baseline = list(numpy.round(baseline, 6))
+
+            # remove 0 in baseline to aviod affecting the p-value
+            list0 = []
+            for i in range(0,len(baseline)):
+                if (baseline[i] == 0):
+                    list0.append(i)
+            baseline = numpy.delete(baseline, list0)
+            # if the -1 signal all is 0 ,skip it
+            if (sum(baseline)==0):
+                continue
+            repeat_box,value_b,value_bIn= R_box(int(start), int(same), frame)
+            repeat_box=list(repeat_box)
+            intensity=(value_b*100/value_all0)-(value_allfs*100/value_all0)
+
+            #length judgenment : the length of repeat box and baseline must be more 1
+            if (len(baseline)<3 or  len(repeat_box)<3):
+                continue
+            if (stats.levene(baseline,repeat_box)[1]>0.05):
+                p=stats.ttest_ind(baseline,repeat_box)[1]
+            else:
+                p = stats.ttest_ind(baseline, repeat_box,equal_var=False)[1]
+            foldchange=round(numpy.mean(repeat_box)/numpy.mean(baseline),3)
+            for i in range(0,len(repeat_box)):
+                repeat_box[i]=str(repeat_box[i])
+            inf.writelines("\t".join([id,motif,start,stop,same,str(in_codon),str(e_count),str(num),str(rbox_in_ecodon),str(r_Len),str(length),str(value_bIn),str(p),str(foldchange),str(intensity)])+"\n")
+            box.writelines("\t".join([id,motif,start,stop]+repeat_box)+"\n")
+            fs.writelines("\t".join([id,motif,start,stop,str(p),str(foldchange)])+"\n")
+            if id+"_"+same in H.keys() and len(H[id+"_"+same])>2:
+                up_inframe,down_inframe=down_up(int(start),int(stop),0)
+                up_outframe, down_outframe = down_up(int(start),int(stop), frame)
+                #normalize up and down respectively
+                up_outframe=numpy.round(up_outframe/numpy.mean(up_inframe),3)
+                down_outframe = numpy.round(down_outframe / numpy.mean(down_inframe),3)
+                up_outframe=numpy.nan_to_num(up_outframe)
+                down_outframe=numpy.nan_to_num(down_outframe)
+                foldchange_2th=round((numpy.sum(down_outframe)+1)/(numpy.sum(up_outframe)+1),3)
+                second.write("\t".join([id,motif,start,stop,same,str(p),str(foldchange),str(foldchange_2th)])+"\n")
+    box.close()
+    fs.close()
+    hsc.close()
+    inf.close()
+    second.close()
+#run
+file=open(sample+"/b_align/deadapter.txt",'r').read().splitlines()
+for j in file:
+    try:
+        os.mkdir(sample+"/b_align/noscreen/FS/"+j)
+    except:
+        print("file exists!")
+    pro_bg(j,sample)
+    print("===================finish "+j+" !===================")
+
